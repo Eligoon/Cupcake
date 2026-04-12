45b059a803c7397eaee6f9fcb85634a92be94674
Eligoon <derpepind117@live.dk>
2026-04-12 12:44:34 +0200

    setup with javalin

--- src/main/java/dk/ek/Main.java
+++ src/main/java/dk/ek/Main.java
@@ -1,8 +1,31 @@
 package dk.ek;
 
+import dk.config.SessionConfig;
+import dk.config.ThymeleafConfig;
+import dk.persistence.ConnectionPool;
+import io.javalin.Javalin;
+import io.javalin.rendering.template.JavalinThymeleaf;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
+    private static final String USER = "postgres";
+    private static final String PASSWORD = "postgres";
+    private static final String URL = "jdbc:postgresql://localhost:5432/%s?currentSchema=public";
+    private static final String DB = "cupcake";
+
+    private static final ConnectionPool connectionPool = ConnectionPool.getInstance(USER, PASSWORD, URL, DB);
+
     public static void main(String[] args) {
+        // Initializing Javalin and Jetty webserver
+        Javalin javApp = Javalin.create(config -> {
+            config.staticFiles.add("/public");
+            config.jetty.modifyServletContextHandler(handler -> handler.setSessionHandler(SessionConfig.sessionConfig()));
+            config.fileRenderer(new JavalinThymeleaf(ThymeleafConfig.templateEngine()));
+            config.staticFiles.add("/templates");
+        }).start(7070);
+
+
+        // Routing
     }
 }
