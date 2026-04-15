8a02779fc8d9fdbae750e7250f760009feb6f43e
LoveBytes25 <keramsby@gmail.com>
2026-04-16 00:49:44 +0200

    added getters

--- src/main/java/dk/entities/Order.java
+++ src/main/java/dk/entities/Order.java
@@ -3,6 +3,8 @@ package dk.entities;
 import java.time.LocalDateTime;
 
 public class Order {
+    private String customerName;
+    private double total;
     private int orderId;
     private int userId;
     private LocalDateTime createdAt;
@@ -13,5 +15,17 @@ public class Order {
         this.userId = userId;
         this.createdAt = createdAt;
         this.status = status;
+    }
+
+    public int getOrderId() {
+        return orderId;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public String getStatus() {
+        return status;
     }
 }
